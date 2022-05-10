964b3edb83f831fd3c8231c810347ed31270af4d
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2022-05-10 15:58:10 +1000

    delete local azurite files

--- AzureUtils.Tests/Tests.fs
+++ AzureUtils.Tests/Tests.fs
@@ -8,6 +8,18 @@ open System.IO
 
 [<TestClass>]
 type TestClass () =
+    
+    let connectionString = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
+    let containerName = "test"
+    let blobName = "test.json"
+
+    [<TestMethodAttribute>]
+    member this.TestUpload () =
+        let testJson = """ {"test": "value"} """
+        let r = BlobUtils.writeJsonBlobAsync(connectionString,containerName,blobName, testJson).Result
+        match r with
+        | Ok() -> Assert.IsNotNull r
+        | _ -> Assert.Fail()
 
     [<TestMethod>]
     member this.TestBlobRetrieval () =
@@ -16,11 +28,25 @@ type TestClass () =
             use sr = new StreamReader(s)
             sr.ReadToEnd()
 
-        let connectionString = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
-        let r = BlobCache.getBlobAsync(connectionString,"conditions","conditions.json").Result
-        match r with 
-        | Ok(c) -> printfn "%s" (readAsString c.Content)
-        | _ -> Assert.Fail("Make sure local storage emulator or Azurite is running.")
+        try 
+            let r = BlobUtils.getBlobAsync(connectionString,containerName, blobName).Result
+            match r with 
+            | Ok(c) -> printfn "%s" (readAsString c.Content)
+            | _ -> Assert.Fail()
+            
+        with
+            | ex -> Assert.Fail("Make sure local storage emulator or Azurite is running with -skipApiVersionCheck flag in correct working folder.")
 
         
+    [<TestMethod>]
+    member this.TestLastModified () =
+        try
+            let r = BlobUtils.getLastModified(connectionString, containerName, blobName).Result
+            match r with
+            | Ok(r) -> printfn "%A" r
+            | _ -> Assert.Fail()
+        with
+            | ex -> Assert.Fail()
 
+    
+   
--- Library.fs
+++ Library.fs
@@ -1,14 +1,24 @@
 ﻿namespace AzureUtils
 open Azure.Storage.Blobs
+open Azure.Storage.Blobs.Models
 open Azure
 
 
-module BlobCache =
+module BlobUtils =
+    
+    let createBlobClient(connectionString : string, containerName : string, blobName : string) =
+         let blobServiceClient = new BlobServiceClient(connectionString, new BlobClientOptions())
+         let blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName)
+         let blobClient = blobContainerClient.GetBlobClient(blobName)
+         blobClient
+    
+    let createBlockBlobClient(connectionString : string, containerName : string, blobName : string) =
+         new Azure.Storage.Blobs.Specialized.BlockBlobClient(connectionString,containerName,blobName)
+         
+    
     let getBlobAsync(connectionString: string, containerName: string, blobName: string) = 
         task {
-            let blobServiceClient = new BlobServiceClient(connectionString, new BlobClientOptions())
-            let blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName)
-            let blobClient = blobContainerClient.GetBlobClient(blobName)
+            let blobClient = createBlobClient(connectionString, containerName, blobName)
             let! blobResultWithMatadata  =
                 async {
                     try
@@ -21,5 +31,48 @@ module BlobCache =
             return blobResultWithMatadata
         }
     
+    let getLastModified(connectionString : string, containerName: string, blobName : string) =
+        task {
+            let blobClient = createBlobClient(connectionString, containerName, blobName)
+            let! lastModified =
+                async {
+                    try
+                        let! metaDataResponse = blobClient.GetPropertiesAsync() |> Async.AwaitTask 
+                        return Ok(metaDataResponse.Value.LastModified)
+                    with
+                        | :? RequestFailedException -> return Error "Could not get blob metadata"
+                
+                }
+
+            return lastModified
+        }
+
+    let writeJsonBlobAsync(connectionString : string, containerName: string, blobName : string, json : string ) =
+        task {
+            let blobServiceClient = new BlobServiceClient(connectionString, new BlobClientOptions())
+            let blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName)
+            let blobClient = blobContainerClient.GetBlobClient(blobName)
+            let! writeResult =
+                async {
+                    // create container if does not exist
+                    
+                    let options = new BlobUploadOptions(HttpHeaders = new BlobHttpHeaders(ContentEncoding = "utf-8",ContentType = "application/json") )
+                    let stringAsBytes = System.Text.Encoding.UTF8.GetBytes(json)
+                    use ms = new System.IO.MemoryStream(stringAsBytes)
+                    
+                    try
+                        let! createIfNotExistsResponse = blobContainerClient.CreateIfNotExistsAsync() |> Async.AwaitTask 
+                        
+                        let! azureResponse = blobClient.UploadAsync(ms,options) |> Async.AwaitTask
+                        return Ok()
+                    with
+                        | ex -> return Error ex
+                }
+            return writeResult
+         }
+
+    
+
+        
 
 
